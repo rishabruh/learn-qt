63c752cca8e1780bc303f2ffb6db101f9036332a
rishabruh <rishabh.agg.2005@gmail.com>
2026-06-16 10:51:41 +0530

    Fixed app

--- Intermediate/firstapp/App/main.py
+++ Intermediate/firstapp/App/main.py
@@ -1,12 +1,13 @@
 import sys
+from pathlib import Path
 
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQuick import QQuickView
 
-if __name__ == "main":
+if __name__ == "__main__":
     app = QGuiApplication()
     view = QQuickView()
-    view.engine().addImportPath(sys.path[0])
+    view.engine().addImportPath(str(Path(__file__).resolve().parent.parent))
     view.loadFromModule("App", "Main")
     view.setResizeMode(QQuickView.ResizeMode.SizeRootObjectToView)
     view.show()
