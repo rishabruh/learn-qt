5042b1082910bde728644e5e3bc557fd464513da
rishabruh <rishabh.agg.2005@gmail.com>
2026-06-16 11:20:30 +0530

    Added bridge code

--- Intermediate/qmlLoader/main.py
+++ Intermediate/qmlLoader/main.py
@@ -7,6 +7,39 @@ from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QmlElement, QQmlApplicationEngine
 from PySide6.QtQuickControls2 import QQuickStyle
 
+QML_IMPORT_NAME = "io.qt.textproperties"
+QML_IMPORT_MAJOR_VERSION = 1
+
+@QmlElement
+class Bridge(QObject):
+
+    @Slot(str, result=str)
+    def getColor(self, s):
+        if s.lower() == "red":
+            return "#ef9a9a"
+        if s.lower() == "green":
+            return "#8ACE00"
+        if s.lower() =="blue:
+            return "#90caf9"
+        return "white"
+
+    @Slot(float, result = int)
+    def getSize(self, s):
+        size = int (s*34)
+        return max(1, size)
+
+    @Slot(str, result=bool)
+    def getItalic(self, s):
+        return s.lower() == "italic"
+
+    @Slot(str, result=bool)
+    def getBold(self, s):
+        return s.lower() == "bold"
+
+    @Slot(str, result=bool)
+    def getUnderline(self, s):
+        return s.lower() == "underline"
+
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     QQuickStyle.setStyle("Material")
